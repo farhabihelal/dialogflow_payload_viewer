319539699fabb0b7bed81019b7348b0e6700d601
Farhabi Helal <farhabi.helal@jp.honda-ri.com>
2023-04-06 16:59:55 +0900

    Added new ES render style.

--- src/dialogflow_payload_viewer/es_visualizer.py
+++ src/dialogflow_payload_viewer/es_visualizer.py
@@ -20,7 +20,7 @@ from es_parser import ESParser
 from graphviz import Digraph
 
 from base_visualizer import BaseVisualizer
-from node_definitions import get_node_def_basic
+from node_definitions import get_node_def_basic, get_node_def_advanced
 
 
 class ESVisualizer(BaseVisualizer):
@@ -181,7 +181,7 @@ class ESVisualizer(BaseVisualizer):
         return url
 
     def get_node_definition(self, node: Intent, **kwargs) -> str:
-        return get_node_def_basic(
+        return get_node_def_advanced(
             node,
             style_data=self.config["style_data"],
             url=self.get_url(node, kwargs["language"]),
@@ -207,9 +207,10 @@ if __name__ == "__main__":
         "base_url": {
             # v1
             # "english": "https://docs.google.com/spreadsheets/d/16jQ8q7M72dBdkxpcIKPXT1nRQbmD4wibZIQRgN_84X8/edit#",
+            # "spanish": "https://docs.google.com/spreadsheets/d/1-VE3Rw25G_Z3DKpYPCcg-jmix3oUf2JdVMDLR6FJhOs/edit#",
             # v2
             "english": "https://docs.google.com/spreadsheets/d/1EDO4AebEr8kygh9Bxt_uO0m2jJ-0wITpolYjtKBjo04/edit#",
-            "spanish": "https://docs.google.com/spreadsheets/d/1-VE3Rw25G_Z3DKpYPCcg-jmix3oUf2JdVMDLR6FJhOs/edit#",
+            "spanish": "https://docs.google.com/spreadsheets/d/1Dj7chRyNzytYzF1pDKLDcMKXvcXTqJ_3MkHV9nZzRE0/edit#"
             # Test
             # "english": "https://docs.google.com/spreadsheets/d/1QDuaijqR4I7CFws_kzww6JZS08QAde6i/edit#",
         },
@@ -217,7 +218,8 @@ if __name__ == "__main__":
         "gid_mapping": {
             "1": {
                 "1": {
-                    "gid": "1163585192",
+                    # "gid": "1163585192",      # v1
+                    "gid": "82097703",  # v2
                     "intents": [
                         "topic-intro",
                         "topic-day-one-session-one-names-origins",
@@ -226,7 +228,8 @@ if __name__ == "__main__":
                     ],
                 },
                 "2": {
-                    "gid": "919985165",
+                    # "gid": "919985165",       # v1
+                    "gid": "1356173353",  # v2
                     "intents": [
                         "topic-day-one-session-two-intro",
                         "topic-travel-homecountry",
@@ -258,80 +261,80 @@ if __name__ == "__main__":
                     ],
                 },
             },
-            "3": {
-                "1": {
-                    "gid": "81336119",
-                    "intents": [
-                        "topic-day-three-session-one-intro",
-                        "topic-day-three-food",
-                        "topic-day-three-session-one-transition",
-                        "topic-birthday",
-                        "topic-day-three-session-one-outro",
-                    ],
-                },
-                "2": {
-                    "gid": "1627598687",
-                    "intents": [
-                        "topic-day-three-session-two-intro",
-                        "topic-sports",
-                        "topic-day-three-session-two-transition",
-                        "topic-day-three-hobbies",
-                        "topic-day-three-session-two-outro",
-                    ],
-                },
-            },
-            "4": {
-                "1": {
-                    "gid": "592896325",
-                    "intents": [
-                        "topic-day-four-session-one-intro",
-                        "topic-day-four-school",
-                        "topic-day-four-session-one-transition",
-                        "topic-day-four-friends",
-                        "topic-day-four-session-one-outro",
-                    ],
-                },
-                "2": {
-                    "gid": "804812225",
-                    "intents": [
-                        "topic-day-four-session-two-intro",
-                        "topic-language",
-                        "topic-day-four-session-two-transition",
-                        "topic-music",
-                        "topic-day-four-session-two-outro",
-                    ],
-                },
-            },
-            "5": {
-                "1": {
-                    "gid": "1381156119",
-                    "intents": [
-                        "topic-day-five-session-one-intro",
-                        "topic-day-five-weather",
-                        "topic-day-five-session-one-transition",
-                        "topic-day-five-clothing",
-                        "topic-day-five-session-one-outro",
-                    ],
-                },
-                "2": {
-                    "gid": "148830916",
-                    "intents": [
-                        "topic-day-five-session-two-intro",
-                        "topic-day-five-travel",
-                        "topic-day-five-session-two-transition",
-                        "topic-olympics",
-                        "topic-day-five-session-two-outro",
-                    ],
-                },
-                "3": {
-                    "gid": "2048883617",
-                    "intents": [
-                        "topic-day-five-session-three-intro",
-                        "topic-day-five-session-three-poem",
-                        "topic-day-five-session-three-outro",
-                    ],
-                },
-            },
+            # "3": {
+            #     "1": {
+            #         "gid": "81336119",
+            #         "intents": [
+            #             "topic-day-three-session-one-intro",
+            #             "topic-day-three-food",
+            #             "topic-day-three-session-one-transition",
+            #             "topic-birthday",
+            #             "topic-day-three-session-one-outro",
+            #         ],
+            #     },
+            #     "2": {
+            #         "gid": "1627598687",
+            #         "intents": [
+            #             "topic-day-three-session-two-intro",
+            #             "topic-sports",
+            #             "topic-day-three-session-two-transition",
+            #             "topic-day-three-hobbies",
+            #             "topic-day-three-session-two-outro",
+            #         ],
+            #     },
+            # },
+            # "4": {
+            #     "1": {
+            #         "gid": "592896325",
+            #         "intents": [
+            #             "topic-day-four-session-one-intro",
+            #             "topic-day-four-school",
+            #             "topic-day-four-session-one-transition",
+            #             "topic-day-four-friends",
+            #             "topic-day-four-session-one-outro",
+            #         ],
+            #     },
+            #     "2": {
+            #         "gid": "804812225",
+            #         "intents": [
+            #             "topic-day-four-session-two-intro",
+            #             "topic-language",
+            #             "topic-day-four-session-two-transition",
+            #             "topic-music",
+            #             "topic-day-four-session-two-outro",
+            #         ],
+            #     },
+            # },
+            # "5": {
+            #     "1": {
+            #         "gid": "1381156119",
+            #         "intents": [
+            #             "topic-day-five-session-one-intro",
+            #             "topic-day-five-weather",
+            #             "topic-day-five-session-one-transition",
+            #             "topic-day-five-clothing",
+            #             "topic-day-five-session-one-outro",
+            #         ],
+            #     },
+            #     "2": {
+            #         "gid": "148830916",
+            #         "intents": [
+            #             "topic-day-five-session-two-intro",
+            #             "topic-day-five-travel",
+            #             "topic-day-five-session-two-transition",
+            #             "topic-olympics",
+            #             "topic-day-five-session-two-outro",
+            #         ],
+            #     },
+            #     "3": {
+            #         "gid": "2048883617",
+            #         "intents": [
+            #             "topic-day-five-session-three-intro",
+            #             "topic-day-five-session-three-poem",
+            #             "topic-day-five-session-three-outro",
+            #         ],
+            #     },
+            # },
         },
         "range_column": {
             "start": "B",
@@ -339,7 +342,7 @@ if __name__ == "__main__":
         },
     }
 
-    from styles import es_style_data
+    from styles import es_style_data, es_dev_style_data
 
     base_dir = os.path.abspath(f"{os.path.dirname(__file__)}/../../")
     agent_dir = os.path.join(base_dir, ".temp/keys")
@@ -351,8 +354,9 @@ if __name__ == "__main__":
         "credential": f"{agent_dir}/haru-test.json",
         "icons_path": f"{base_dir}/icons",
         "render_path": f"{base_dir}/renders/ES-Demo",
-        "parse_filepath": f"{data_dir}/ES_GS.xlsx",
-        "style_data": es_style_data,
+        "parse_filepath": f"{data_dir}/ES-Demo/en/ES_GS_D2S1.xlsx",
+        # "parse_filepath": f"{data_dir}/ES-Demo/es/ES_GS_es.xlsx",
+        "style_data": es_dev_style_data,  # es_style_data,
         "sheet_data": sheet_data,
         "language_code": "en",
     }
--- src/dialogflow_payload_viewer/styles.py
+++ src/dialogflow_payload_viewer/styles.py
@@ -371,3 +371,139 @@ es_style_data = {
     #     },
     # },
 }
+
+es_dev_style_data = {
+    "default": {
+        "intent-name": {
+            "color": "palegreen3",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "action": {
+            "color": "darkseagreen4",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "messages": {
+            "color": "burlywood1",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+    },
+    "fallback": {
+        "intent-name": {
+            "color": "coral",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "action": {
+            "color": "darkseagreen4",
+            "font-size": "16",
+            "font": "Calibri",
+        },
+        "messages": {
+            "color": "burlywood1",
+            "font-size": "18",
+            "font": "Calibri",
+        },
+    },
+    "edge": {
+        "direct": {
+            "color": "black",
+            "arrowsize": "2.0",
+            "penwidth": "3.0",
+            "style": "",
+        },
+        "indirect": {
+            "color": "firebrick2",
+            "arrowsize": "2.0",
+            "penwidth": "3.0",
+            "style": "",
+        },
+    },
+    "question": {
+        "intent-name": {
+            "color": "mediumslateblue",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "action": {
+            "color": "darkseagreen4",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "messages": {
+            "color": "burlywood1",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+    },
+    "answer": {
+        "intent-name": {
+            "color": "palegreen3",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "action": {
+            "color": "darkseagreen4",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "messages": {
+            "color": "burlywood1",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+    },
+    "answer-question": {
+        "intent-name": {
+            "color": "mediumslateblue",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "action": {
+            "color": "darkseagreen4",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "messages": {
+            "color": "burlywood1",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+    },
+    "disabled": {
+        "intent-name": {
+            "color": "azure4",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "action": {
+            "color": "darkseagreen4",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+        "messages": {
+            "color": "burlywood1",
+            "font-size": "20",
+            "font": "Calibri",
+        },
+    },
+    # "repeat": {
+    #     "intent-name": {
+    #         "color": "orangered1",
+    #         "font-size": "20",
+    #         "font": "Calibri",
+    #     },
+    #     "action": {
+    #         "color": "darkseagreen4",
+    #         "font-size": "20",
+    #         "font": "Calibri",
+    #     },
+    #     "messages": {
+    #         "color": "burlywood1",
+    #         "font-size": "20",
+    #         "font": "Calibri",
+    #     },
+    # },
+}
